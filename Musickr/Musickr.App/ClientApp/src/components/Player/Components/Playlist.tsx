f561833803d1bb329c8144d6c93ec1710df91f88
LouisLeGris <35083014+DeerisLeGris@users.noreply.github.com>
2023-09-09 20:56:02 +0200

    Playlist.tsx updated

--- Musickr/Musickr.App/ClientApp/src/components/Player/Components/Playlist.tsx
+++ Musickr/Musickr.App/ClientApp/src/components/Player/Components/Playlist.tsx
@@ -1,13 +1,13 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useCallback, useState} from "react";
 import {useDebounce} from "react-use";
 import {Button, Divider, Heading, VStack} from "@chakra-ui/react";
 import {ExternalLinkIcon} from "@chakra-ui/icons";
 import SearchBar from "../../Search/Components/SearchBar";
 import Track from "./Track";
+import {createSearchParams, useNavigate} from "react-router-dom";
+import {StringParam, useQueryParam} from "use-query-params";
 
 type PlaylistProps = {
-  defaultValue?: string;
-  onChange:      (value: string) => void;
   tracks: {
     author:     string,
     title:      string,
@@ -24,25 +24,21 @@ type PlaylistProps = {
 };
 
 const Playlist = ({
-  defaultValue="",
-  onChange,
   tracks
 }: PlaylistProps) => {
+  const [place, setPlace] = useQueryParam("place", StringParam);
+  
+  const navigate = useNavigate();
 
-  const [searchContent, setSearchContent] = useState(defaultValue);
-  const [searchContentDebounced, setSearchContentDebounced] = useState("");
+  const onSearchBarChange = useCallback((value: string) => {
+    const params = { place: value };
 
-  const handleInput = (changeEvent: ChangeEvent<HTMLInputElement>) => {
-    setSearchContent(changeEvent.target.value);
-  };
-
-  const [, cancel] = useDebounce(
-    () => {
-      setSearchContentDebounced(searchContent);
-    },
-    500,
-    [searchContent]
-  );
+    navigate({
+      pathname: "/player",
+      search: `?${createSearchParams(params)}`
+    })
+  },
+  [navigate]);
   
   return (
     <VStack
@@ -55,7 +51,10 @@ const Playlist = ({
     >
       <VStack p="5%" w="100%">
         <Heading variant='playlist'>Musickr</Heading>
-        <SearchBar onChange={onChange} defaultValue={defaultValue} /> {/* need to modify this component to allow styling */}
+        <SearchBar 
+          onChange={onSearchBarChange} 
+          defaultValue={place} 
+        />
       </VStack>
       <Divider w="90%" />
       <VStack w="100%" spacing="0" overflowX={"hidden"} overflowY={"scroll"} paddingTop="5px">
